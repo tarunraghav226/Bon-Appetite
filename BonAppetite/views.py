2424e909d63af7e1995e1e6b6adebc6fa6383d79
Naman Gupta <48116263+nmn-gupta@users.noreply.github.com>
2021-04-07 18:28:53 +0530

    Naman (#4)
    * created home page
    
    * added styling for home page in static folder
    
    * created url for home page and add corresponding view
    
    * saved changes
    
    * created templates for login and register and link them to nav bar
    
    * added code for sign up and maps it to urls.py
    
    * created login view and maps it with urls.py
    
    * completed code for logout view
    
    * save changes

---
+++ BonAppetite/views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User, auth
+from django.shortcuts import redirect, render
+from django.views import View
+
+
+def home(request):
+    return render(request, "home.html", {})
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "register.html", {})
+
+    def post(self, request):
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password1,
+        )
+        user.save()
+        return redirect("/")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            return redirect("/")
+
+
+class Logout(View):
+    def get(self, request):
+        auth.logout(request)
+        return redirect("/")
