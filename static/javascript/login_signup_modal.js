08d57f9536ad5bfa5de946671abb5d7b4a97f3d6
Tarun <49776598+tarunraghav226@users.noreply.github.com>
2021-04-12 10:55:36 +0530

    Tarun (#5)
    * init commit
    
    * Added app in INSTALLED_APPS list.
    
    * Added models.
    
    * Updated relationship of UserDetails model with User model.
    
    * Updated base and home page.
    
    * Added login and sign up modal
    
    * Added login and logout functionality.
    
    * Added user helper.

---
+++ static/javascript/login_signup_modal.js
@@ -0,0 +1,64 @@
+const login = document.getElementById("login");
+const signup = document.getElementById("signup");
+
+const loginForm = document.querySelector(".form--login");
+const modalOverlay = document.querySelector(".overlay--modal");
+const closeModalBtn =  document.querySelector(".close--modal--btn");
+const signUpForm = document.querySelector(".form--signup");
+
+const messageCloseBtn = document.getElementById("message--close--icon");
+
+loginForm.style.display = "none";
+modalOverlay.style.display = "none";
+closeModalBtn.style.display = "none";
+signUpForm.style.display = "none";
+
+closeModalBtn.addEventListener('click', ()=>{
+    loginForm.style.display = "none";
+    modalOverlay.style.display = "none";
+    closeModalBtn.style.display = "none";
+    signUpForm.style.display = "none";
+});
+
+login.addEventListener('click', ()=>{
+    if(modalOverlay.style.display == "block" && 
+        signUpForm.style.display !== "block"){
+        closeModalBtn.style.display = "none";
+        modalOverlay.style.display = "none";
+    }
+    else{
+        modalOverlay.style.display = "block";
+        closeModalBtn.style.display = "block";
+    }
+
+    signUpForm.style.display = "none";
+
+    if(loginForm.style.display == "block")
+        loginForm.style.display = "none";
+    else 
+        loginForm.style.display = "block";
+
+});
+
+signup.addEventListener('click', ()=>{
+    if(modalOverlay.style.display == "block" && 
+        loginForm.style.display !== "block"){
+        closeModalBtn.style.display = "none";
+        modalOverlay.style.display = "none";
+    }
+    else{
+        modalOverlay.style.display = "block";
+        closeModalBtn.style.display = "block";
+    }
+
+    loginForm.style.display = "none";
+
+    if(signUpForm.style.display == "block")
+        signUpForm.style.display = "none";
+    else 
+        signUpForm.style.display = "block";
+});
+
+messageCloseBtn.addEventListener("click", ()=>{
+    document.querySelector(".messages").style.display = "none";
+});
